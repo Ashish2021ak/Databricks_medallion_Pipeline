015b9857a0232e6dd51a419416c2cb1465c39db3
ASHISH KUMAR MOHAPATRA <54214301+Ashish2021ak@users.noreply.github.com>
2026-03-19 15:04:09 +0530

    Update gold_notebook.py

--- notebooks/gold_notebook.py
+++ notebooks/gold_notebook.py
@@ -1,1 +1,26 @@
+silver_path="/Volumes/workspace/default/my_volume/Silver_layer"
+gold_path="/Volumes/workspace/default/my_volume/Gold_layer"
+# read data from Silver Notebook
+#-----------------------------------
+
+df_silver=spark.read.format("delta")\
+    .load(silver_path)
+
+
+# Business analysis ready Transformation
+#---------------------------------------------
+
+from pyspark.sql.functions import col,count,avg
+df_gold=df_silver.groupBy("salaryFlag")\
+    .agg(count("*").alias("employees_count"))
+
+
+#Write to GOLD LAYER
+#-------------------------
+df_gold.write.format("delta")\
+    .mode("overwrite")\
+    .option("overwriteSchema","true")\
+    .save(gold_path)
+
+display(df_gold)
 
